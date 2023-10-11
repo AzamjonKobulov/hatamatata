2d70e7ed0187db00d9c30e7f3c588750b8d1d283
AzamjonKobulov <azamjonkobulov1998@gmail.com>
2023-10-11 19:54:21 +0500

    fix/some issues

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,9 +1,13 @@
 const sidebar = document.querySelector('.sidebar');
 const sideBarMenuBtn = document.querySelector('.sidebar-menu-button');
+const hamburger = document.querySelector('.hamburger');
+const close = document.querySelector('.close');
 const body = document.body;
 
 const toggleSidebarMenu = () => {
   sidebar.classList.toggle('hidden');
+  hamburger.classList.toggle('hidden');
+  close.classList.toggle('hidden');
   body.classList.toggle('overflow-hidden');
 };
 
